836bc60fb50ea7f1cf7d2cf3c7b4361ce6d7a188
ProfAlex2021 <prof.alex.etec.2021@gmail.com>
2021-03-24 22:46:26 -0300

    Primeiro app com 2 telas

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { Button, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" options={{ title : "Home" }} component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const HomeScreen = ({ navigation }) => {
+  return(
+    <Button
+     title="Vamos para a página do Luís"
+     onPress={() =>
+     navigation.navigate('Profile', { name: 'Luís Ernani' }) 
+    } />
+  );
+}
+
+const ProfileScreen = ({ navigation, route}) => {
+  return(
+    <Text>Rota usada: {route.params.name}</Text>
+  );
+}
+
+export default App;
